0c8960026a2d29d27bc0e8c60daf98b5f73c44f3
nenad0707 <risticnenad.vr@gmail.com>
2025-12-23 03:30:02 +0100

    feat: Redesign demo page and cleanup workflows
    - Complete UI redesign with glassmorphism theme
    - New HomeLayout component for landing page
    - Remove GitHub Pages workflow (now using Azure Static Web Apps)
    - Improved responsive design for mobile devices

--- src/BlazzyMotion.Demo/wwwroot/js/scroll.js
+++
@@ -1,6 +0,0 @@
-﻿window.scrollToElement = (elementClass) => {
-    const element = document.querySelector('.' + elementClass);
-    if (element) {
-        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
-};
