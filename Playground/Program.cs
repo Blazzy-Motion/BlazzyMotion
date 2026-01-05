7e0097d495af853f2983b0452e226c736b8a826a
nenad0707 <risticnenad.vr@gmail.com>
2026-01-05 01:53:35 +0100

    feat: updated project with new BlazzyMotion.Carousel prject reference

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -4,7 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents(options =>
+    {
+        options.DetailedErrors = true;
+    });
 
 var app = builder.Build();
 
